47843b0ab80db904de81c6ef0b2d322b50954cd1
snah69420 <108625750+snah69420@users.noreply.github.com>
2025-11-26 17:56:19 +0800

    Update PasswordManager.java
    added comments, finalized, it categorizes the user/pass on one site

--- PasswordManager.java
+++ PasswordManager.java
@@ -1,212 +1,279 @@
-package library; // eclipseIDE-specific line, please remove!
-import java.io.*;				// Majority of the functions like BufferedReader and stuff
-import java.util.ArrayList;		// Arrays
+package library;				// Eclipse IDE-specific line, please remove!
+import java.io.*;				// BufferedReader and read database.txt packages
+import java.util.ArrayList;		// arrays
 import java.util.Scanner;		// yk what this is
 
 public class PasswordManager {
-	public static void main (String[] args) {
-		
+	public static void main(String[] args) {
+
 		Scanner input = new Scanner(System.in);
-		ArrayList<String> user = new ArrayList<>();
-		ArrayList<String> password = new ArrayList<>();
+
+		ArrayList<ArrayList<String>> user = new ArrayList<>();
+		ArrayList<ArrayList<String>> password = new ArrayList<>();
 		ArrayList<String> site = new ArrayList<>();
+
 		ArrayList<String> loginUser = new ArrayList<>();
 		ArrayList<String> loginPass = new ArrayList<>();
+
 		String db = "database.txt";
-		// load the database
-		loadDB(db, user, password, site, loginUser, loginPass);
-		
-		
-		// First-Time signup
-		
+
+		// loads database on execution
+		loadDB(db, site, user, password, loginUser, loginPass);
+
+		// first time signup
 		if (loginUser.isEmpty()) {
 			System.out.println("First-time Setup");
-			
+
 			System.out.print("Enter username: ");
 			loginUser.add(input.nextLine());
-			
-			System.out.print("Enter password: ");
-			String loginPassUnconfirmed = input.nextLine();
-			
-			System.out.print("Enter password: ");
-			String confirm = input.nextLine();
-			
-			if (loginPassUnconfirmed.equals(confirm)) {
-			loginPass.add(loginPassUnconfirmed);
-			System.out.println("Setup finished!");
-			
-			// save data
-		    saveData(db, user, password, site, loginUser, loginPass);
 
-			
+			System.out.print("Enter password: ");
+			String pass = input.nextLine();
+
+			System.out.print("Confirm password: ");
+			String confirm = input.nextLine();
+
+			if (pass.equals(confirm)) {
+				loginPass.add(pass);
+				System.out.println("Setup finished!");
+				saveData(db, site, user, password, loginUser, loginPass);
 			} else {
-				System.out.println("Passwords do not match. Restart the program and try again!");
+				System.out.println("Passwords do not match. Restart the program.");
 				return;
 			}
-		} else { // Already Registered
+		} else {
+			// login
 			System.out.println("Please login.");
-			
 			boolean loggedIn = false;
-					
-		
+
 			while (!loggedIn) {
-			System.out.print("Username: ");
-			String loginUserRegistered = input.nextLine();
-			
-			System.out.print("Password: ");
-			String loginPassRegistered = input.nextLine();
-			
-			String storedUser = loginUser.get(0);
-			String storedPass = loginPass.get(0);
-			
-			if (loginUserRegistered.equals(storedUser) && loginPassRegistered.equals(storedPass)) {
-				System.out.println("Successfully logged in.");
-				loggedIn = true;
-			} else {
-			System.out.println("Username or Password does not match the database, try again!");
+				System.out.print("Username: ");
+				String u = input.nextLine();
+
+				System.out.print("Password: ");
+				String p = input.nextLine();
+
+				if (u.equals(loginUser.get(0)) && p.equals(loginPass.get(0))) {
+					System.out.println("Successfully logged in.");
+					loggedIn = true;
+				} else {
+					System.out.println("Incorrect username or password.");
 				}
 			}
 		}
-		
-		// END OF LOGIN FUNCTION
-		
-		// Main Screen
+
+		// Main menu!!!
 		while (true) {
-		System.out.println("---Welcome to our Password Manager!---");
-		System.out.println("1. Add Credentials");
-		System.out.println("2. View Credentials");
-		System.out.println("3. Delete Credentials");
-		System.out.println("4. Exit");
-		System.out.print("Choice: ");
+			System.out.println("\n---Welcome to Password Manager---");
+			System.out.println("1. Add Credentials");
+			System.out.println("2. View Credentials");
+			System.out.println("3. Delete Credentials");
+			System.out.println("4. Exit");
+			System.out.print("Choice: ");
+			// tried using the int datatype on this, but it just won't work
+			String choice = input.nextLine();
 
-		String choice = input.nextLine();
-		
-		// Add Credentials
-		if (choice.equals("1")) {
-			System.out.println("---Add Credentials---");
-			System.out.print("Enter Username/Email: ");
-			user.add(input.nextLine());
-			
-			System.out.print("Enter Password: ");
-			password.add(input.nextLine());
-			
-			System.out.print("Website?: ");
-			site.add(input.nextLine());
-			
-			System.out.println("Credentials added!");
-			saveData(db, user, password, site, loginUser, loginPass);
+			// add credentials
+			if (choice.equals("1")) {
+				System.out.println("---Add Credentials---");
 
-		// View Credentials
-		} else if (choice.equals("2")) {
-			System.out.println("---View Credentials---");
-			if(user.isEmpty()) {
-				System.out.println("There are no credentials added, yet...");
-			 } else {
-               System.out.println("--- Websites ---");
-               for (int i = 0; i < user.size(); i++) {
-                   System.out.println((i + 1) + ". " + site.get(i));
-               }
-               // Query
-                   System.out.println("Select a website: ");
-                   int query_site = input.nextInt();
-                   int site_index = query_site - 1;
-                   input.nextLine();
-                   System.out.println("--- Credentials on " + site.get(site_index) + " ---");
-                   System.out.println("Username: " + user.get(site_index));
-                   System.out.println("Password: " + password.get(site_index));
-                   
-                   System.out.println("Press enter to return to menu...");
-                   input.nextLine();
-                   
-                   
-			 }
-            //       System.out.println("   Username: " + user.get(i)); 		]
-            //       System.out.println("   Password: " + password.get(i));		]	UNUSED CODE! feel free to delete.
-            //       System.out.println("-------------------------");			]
-         
-			// Remove Credentials
-		} else if (choice.equals("3")) {
-			if (user.isEmpty()) {
-				System.out.println("There are no credentials added, yet...");
-			} else {
-				System.out.println("---Delete Credentials---");
-				for (int i = 0; i < user.size(); i++) {
-					System.out.println((i + 1) + ". " + user.get(i));
+				System.out.print("Website: ");
+				String newSite = input.nextLine();
+
+				System.out.print("Username/Email: ");
+				String newUser = input.nextLine();
+
+				System.out.print("Password: ");
+				String newPass = input.nextLine();
+
+				int index = site.indexOf(newSite);
+
+				if (index != -1) {								// determines if a site already exists
+					// site already exists
+					user.get(index).add(newUser);
+					password.get(index).add(newPass);
+				} else {										// creates a new site
+					site.add(newSite);
+
+					ArrayList<String> uList = new ArrayList<>();
+					ArrayList<String> pList = new ArrayList<>();
+																// add new username/pass to the new list
+					uList.add(newUser);
+					pList.add(newPass);
+
+					user.add(uList);
+					password.add(pList);
 				}
-				System.out.print("Enter a number to delete: ");
-				int delIndex = Integer.parseInt(input.nextLine()) - 1;
 
-				if (delIndex < 0 || delIndex >= user.size()) {
-					System.out.println("Invalid number.");
-				} else {
-					user.remove(delIndex);
-					password.remove(delIndex);
-					site.remove(delIndex);
-					System.out.println("Credential deleted successfully");
-				} 
+				saveData(db, site, user, password, loginUser, loginPass);
+				System.out.println("Credentials added!");
+
+			}
+
+			// VIEW
+			else if (choice.equals("2")) {
+				System.out.println("---View Credentials---");
 				
+				if (site.isEmpty()) {							// no credentials
+					System.out.println("No credentials saved.");
+					continue;									// returns to main menu
 				}
-			} else if (choice.equals("4")) {
-			System.out.println("Exiting.");
-			break;
-		}
+
+				System.out.println("--- Websites ---");
+				for (int i = 0; i < site.size(); i++) {
+					System.out.println((i + 1) + ". " + site.get(i));
+				}
+
+				System.out.print("Select a website: ");
+				int index = Integer.parseInt(input.nextLine()) - 1;
+
+				if (index < 0 || index >= site.size()) {
+					System.out.println("Invalid choice.");
+					continue;
+				}
+				// accounts for <what site?>
+				System.out.println("--- Accounts for " + site.get(index) + " ---");
+				ArrayList<String> uList = user.get(index);
+				ArrayList<String> pList = password.get(index);
+				// lists down
+				for (int i = 0; i < uList.size(); i++) {
+					System.out.println((i + 1) + ". Username: " + uList.get(i));
+					System.out.println("   Password: " + pList.get(i));
+				}
+
+			}
+
+			// DELETE
+			else if (choice.equals("3")) {
+				if (site.isEmpty()) {
+					System.out.println("There are no credentials added... yet!");
+					continue;
+				}
+				// query the websites
+				System.out.println("--- Websites ---");
+				for (int i = 0; i < site.size(); i++) {
+					System.out.println((i + 1) + ". " + site.get(i));
+				}
+
+				System.out.print("Select a website: ");
+				int sIndex = Integer.parseInt(input.nextLine()) - 1;
+
+				if (sIndex < 0 || sIndex >= site.size()) {
+					System.out.println("Invalid choice.");
+					continue;
+				}
+
+				ArrayList<String> uList = user.get(sIndex);
+				ArrayList<String> pList = password.get(sIndex);
+
+				System.out.println("--- Accounts ---");
+				for (int i = 0; i < uList.size(); i++) {
+					System.out.println((i + 1) + ". " + uList.get(i));
+				}
+
+				System.out.print("Select account number to delete: ");
+				int aIndex = Integer.parseInt(input.nextLine()) - 1;
+
+				if (aIndex < 0 || aIndex >= uList.size()) {
+					System.out.println("Invalid number, try again!");
+				} else {
+					uList.remove(aIndex);
+					pList.remove(aIndex);
+
+					if (uList.isEmpty()) {
+						site.remove(sIndex);
+						user.remove(sIndex);
+						password.remove(sIndex);
+					}
+
+					saveData(db, site, user, password, loginUser, loginPass);
+					System.out.println("Deleted successfully!");
+				}
+			}
+
+			// 
+			else if (choice.equals("4")) {
+				System.out.println("Exiting...");
+				break;
+			}
+
+			else {
+				System.out.println("Invalid choice, try again!");
+			}
 		}
 	}
-	
-	// END OF PROGRAM, here are the methods, kinda stolen tho
-	
-	// save to txt thingy
-	public static void saveData(String db, ArrayList<String> user, ArrayList<String> password, ArrayList<String> site, ArrayList<String> loginUser, ArrayList<String> loginPass) {
-		try {
-			File file = new File(db);
-			File parent = file.getParentFile();
-			if (parent != null) {
-			    parent.mkdirs();
+	// END OF PROGRAM, here are the methods
+
+	// saveData method, i still don't understand how this works
+	public static void saveData(String db, ArrayList<String> site, ArrayList<ArrayList<String>> user,
+			ArrayList<ArrayList<String>> password, ArrayList<String> loginUser,
+			ArrayList<String> loginPass) {
+		// writes the credentials into a file
+		try (PrintWriter pw = new PrintWriter(new FileWriter(db))) {
+			
+			if (!loginUser.isEmpty()) {																// shoves the input master login to the database.txt
+				pw.println("Login Credentials |" + loginUser.get(0) + "|" + loginPass.get(0));
 			}
-			try (PrintWriter pw = new PrintWriter(new FileWriter(file))) {
+			for (int i = 0; i < site.size(); i++) {
+				ArrayList<String> uList = user.get(i);
+				ArrayList<String> pList = password.get(i);
 
-				// First-time setup save login
-				if (!loginUser.isEmpty() && !loginPass.isEmpty()) {
-					pw.println("LOGIN|" + loginUser.get(0) + "|" + loginPass.get(0));
-				} // database thingy
-				for (int i = 0; i < user.size(); i++) {
-					String u = user.get(i).replace("|", "/");
-					String p = password.get(i).replace("|", "/");
-					String s = site.get(i).replace("|", "/");
-
-					pw.println(u + "|" + p + "|" + s);
+				for (int j = 0; j < uList.size(); j++) {
+					pw.println(site.get(i) + "|" + uList.get(j) + "|" + pList.get(j));
 				}
 			}
-
-			System.out.println("Database saved successfully.");
 
 		} catch (Exception e) {
-			System.out.println("Error saving database.");
+			System.out.println("Error saving credentials.");
 		}
 	}
-	// loadDB method, loads the database on execution
-	public static void loadDB(String db, ArrayList<String> user, ArrayList<String> password, ArrayList<String> site, ArrayList<String> loginUser, ArrayList<String> loginPass) {
+
+	// loadDB method, idk how it worked
+	public static void loadDB(String db, ArrayList<String> site, ArrayList<ArrayList<String>> user,
+			ArrayList<ArrayList<String>> password, ArrayList<String> loginUser,
+			ArrayList<String> loginPass) {
+
 		try {
 			File file = new File(db);
 			if (!file.exists()) return;
-
+			// reads database
 			try (BufferedReader br = new BufferedReader(new FileReader(file))) {
 				String line;
+
 				while ((line = br.readLine()) != null) {
 					String[] parts = line.split("\\|");
-            	
+
 					if (parts[0].equals("LOGIN")) {
 						loginUser.add(parts[1]);
 						loginPass.add(parts[2]);
-					} else if(parts.length == 3) {
-						user.add(parts[0]);
-						password.add(parts[1]);
-						site.add(parts[2]);
+					} else {
+						String s = parts[0];
+						String u = parts[1];
+						String p = parts[2];
+
+						int index = site.indexOf(s);
+
+						if (index != -1) {
+							user.get(index).add(u);
+							password.get(index).add(p);
+						} else {
+							site.add(s);
+
+							ArrayList<String> uList = new ArrayList<>();
+							ArrayList<String> pList = new ArrayList<>();
+
+							uList.add(u);
+							pList.add(p);
+
+							user.add(uList);
+							password.add(pList);
+						}
 					}
 				}
 			}
+
 		} catch (Exception e) {
-			System.out.println("Error loading database.");
+			System.out.println("Error loading data.");
 		}
 	}
 }
